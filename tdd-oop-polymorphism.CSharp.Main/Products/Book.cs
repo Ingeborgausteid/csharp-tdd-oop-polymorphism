464c9507125b04defac288a86943408ccc041e62
Ingeborg Brommeland Austeid <ingeborg_austeid@hotmail.com>
2024-08-22 10:57:34 +0200

    adds modified source code to include polymorphism

---
+++ tdd-oop-polymorphism.CSharp.Main/Products/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tdd_oop_polymorphism.CSharp.Main;
+
+namespace Products
+{
+    public class Book : IProduct
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public Book(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+       
+    }
+}
---
+++ tdd-oop-polymorphism.CSharp.Main/Products/Drink.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tdd_oop_polymorphism.CSharp.Main;
+
+namespace Products
+{
+    public class Drink : IProduct
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public Drink(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
